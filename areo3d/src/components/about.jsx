2de01f777ff242a67475954c8d400275e3918260
Elias Lopes <ealopes@aggies.ncat.edu>
2025-02-07 15:55:51 -0500

    vbnc

--- areo3d/src/components/about.jsx
+++ areo3d/src/components/about.jsx
@@ -1,35 +1,52 @@
-import brian from '../assets/brian.png'
 
-
+import { FaLaptop } from "react-icons/fa";
+import { Canvas } from "@react-three/fiber";
+import { Float, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import Laptop from "../3D Models/laptop";
+import { AmbientLight } from "three";
 export function AboutUs(){
 
 
-    return <div className="w-[100vw] h-fit bg-gray-50 flex items-center md:mt-[10vh] justify-start text-left gap-[50px] md:p-[80px] ">
+    return <div className="w-[100vw] h-[60vh] flex items-start mt-[10vh] justify-start text-left flex-wrap ">
 
-            <div className='flex flex-col gap-[20px] justify-start border-gray-500 w-full translate-x-[11vw] p-[20px]
-             md:w-[40%] rounded-[20px] bg-white border-[.5px] b'>
-                <h1 className='text-4xl'>About Us</h1>
+            <div className='flex flex-col gap-[25px] items-center justify-center w-full md:h-full h-fit md:p-[0px] 
+             lg:w-[50%]  bg-black p-[10px] pb-[20px] pt-[20px]  '>
+                <h1 className='text-5xl md:text-5xl lg:text-7xl text-white'>About Us</h1>
 
-                <h2>
-                Building Your Online Presence in Three Dimensions
-
+                <h2 className='text-white text-[13px] md:text-[15px]'>
+                    Building Your Online Presence in Three Dimensions
                 </h2>
 
-                <p className='w-[80%] text-gray-400'>
-                I founded AERO3D to help businesses succeed in today’s online world. With 15 years of experience in hospitality management and 
-                four years in 
-                online sales during the COVID-19 pandemic, I learned how to connect with people and adapt to challenges.
-                AERO3D Marketing bridges the gap between strategy and execution, providing businesses with high-quality web design, digital marketing,
-                and optimization. My mission is to empower businesses with cutting-edge digital tools and strategies tailored to their needs.
-                When I’m not working, I enjoy exploring new technology trends and spending time with my family. I look forward to helping your business grow.
-
+                <p className='md:w-[85%] w-[90%] text-gray-400 text-[14px] md:text-[16px] lg:text-[17px]'>
+                    I founded AERO3D to help businesses succeed in today’s online world. With 15 years of experience in hospitality management and 
+                    four years in 
+                    online sales during the COVID-19 pandemic, I learned how to connect with people and adapt to challenges.
+                    AERO3D Marketing bridges the gap between strategy and execution, providing businesses with high-quality web design, digital marketing,
+                    and optimization. My mission is to empower businesses with cutting-edge digital tools and strategies tailored to their needs.
+                    When I’m not working, I enjoy exploring new technology trends and spending time with my family. I look forward to helping your business grow.
                 </p>
                 
-                <p>
-                Your vision. Our expertise. Let’s build something great together.
+                <p  className='text-white text-[13px] md:text-[15px]'>
+                    Your vision. Our expertise. Let’s build something great together.
                 </p>
 
             </div>  
+
+            <div className=" w-full h-fit lg:w-[50%] lg:h-full ">
+
+                   <Canvas className="w-full h-full">
+                    <PerspectiveCamera position={[0,0,0]}/>
+                    <ambientLight intensity={2}/>
+                    <Float floatIntensity={1} rotationIntensity={1} speed={3}>
+
+                   
+                    <Laptop position={[0,-10,-25]}  />
+                    </Float>
+                   </Canvas>
+
+
+
+            </div>
 
 
     </div>
