ca0e11bcbc0060bcb25a44b7d4604933dc92e055
Eliaslopes248 <ealopes@aggies.ncat.edu>
2025-02-08 15:26:56 -0500

    b

--- areo3d/src/components/about.jsx
+++ areo3d/src/components/about.jsx
@@ -1,37 +1,18 @@
-
-import { FaLaptop } from "react-icons/fa";
-import { Canvas } from "@react-three/fiber";
-
-export function AboutUs(){
-
-
-    return <div className="w-full h-[60vh] size-fit flex  mt-[10vh]  ">
-
-            <div className='flex flex-col gap-[25px] items-center justify-center w-full md:h-full h-fit 
-              bg-black pb-[40px] pt-[40px] rounded-[40px]  '>
-                <h1 className='text-5xl md:text-5xl lg:text-7xl text-white'>About Us</h1>
-
-                <h2 className='text-white text-[14px] w-[90%] md:w-fit md:text-[15px] ml-[0vw]'>
-                    Building Your Online Presence in Three Dimensions
-                </h2>
-
-                <p className='md:w-[55%] w-[90%]  text-gray-400 text-[15px] md:text-[16px] lg:text-[17px]'>
-                    I founded AERO3D to help businesses succeed in today’s online world. With 15 years of experience in hospitality management and 
-                    four years in 
-                    online sales during the COVID-19 pandemic, I learned how to connect with people and adapt to challenges.
-                    AERO3D Marketing bridges the gap between strategy and execution, providing businesses with high-quality web design, digital marketing,
-                    and optimization. My mission is to empower businesses with cutting-edge digital tools and strategies tailored to their needs.
-                    When I’m not working, I enjoy exploring new technology trends and spending time with my family. I look forward to helping your business grow.
-                </p>
-                
-                <p  className='text-white text-[15px] hidden md:text-[15px]'>
-                    Your vision. Our expertise. Let’s build something great together.
-                </p>
-
-            </div>  
-
-        
-    </div>
-
-
-}
+export function AboutUs() {
+    return (
+      <div className="w-full h-[60vh] size-fit flex mt-[10vh] mb-[10vh]">
+        <div className='flex flex-col gap-[25px] items-center justify-center w-full h-fit bg-black p-[5%] rounded-[40px]'>
+          <h1 className='text-5xl md:text-5xl lg:text-5xl text-white'>About Us</h1>
+          <h2 className='text-white text-[14px] w-[90%] md:w-fit md:text-[15px] ml-[0vw]'>
+            Building Your Online Presence in Three Dimensions
+          </h2>
+          <p className='md:w-[55%] w-[90%] text-gray-400 text-[15px] md:text-[16px] lg:text-[16px]'>
+            I founded AERO3D to help businesses succeed in today’s online world. With 15 years of experience in hospitality management and four years in online sales during the COVID-19 pandemic, I learned how to connect with people and adapt to challenges. AERO3D Marketing bridges the gap between strategy and execution, providing businesses with high-quality web design, digital marketing, and optimization. My mission is to empower businesses with cutting-edge digital tools and strategies tailored to their needs. When I’m not working, I enjoy exploring new technology trends and spending time with my family. I look forward to helping your business grow.
+          </p>
+          <p className='text-white text-[15px] hidden md:text-[15px]'>
+            Your vision. Our expertise. Let’s build something great together.
+          </p>
+        </div>
+      </div>
+    );
+  }
--- areo3d/src/components/footer.jsx
+++ areo3d/src/components/footer.jsx
@@ -1,43 +1,10 @@
 import React from 'react';
 import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa';
 
-
-
-
-export function Footer(){
-    return <div className="w-full items-center md:pl-[20px] h-fit mt-[0vh] border-0 md:h-[250px] p-[15px] pb-[30px] flex flex-wrap gap-[10px]">
-            
-                <div className=" w-[80%] md:w-fit h-fit p-[10px]  ">
-                        <p className='text-gray-400 md:text-lg text-[13px] md:mb-[20px]'>
-                            Copyright ©2025 ÆRO3D. All rights reserved.
-                        </p>
-                </div>
-
-                <div className=" w-[80%] md:w-fit h-fit p-[10px]">
-                    <p className='text-gray-400 md:text-lg text-[13px] md:mb-[20px]'>
-                    Term of Services  Privacy Policy  Cookie Policy
-                    </p>
-                </div>
-
-                <div className=" w-[85%] md:w-fit md:ml-auto md:mr-[2vw] h-fit p-[10px] flex flex-wrap gap-[15px] md:gap-[40px] ">
-                        <a href="">
-                            <FaFacebook className='text-black size-8' />
-                        </a>
-
-                        <a href="">
-                            <FaInstagram className='text-black size-8' />
-                        </a>
-
-                        <a href="">
-                            <FaLinkedin className='text-black size-8' />
-                        </a>
-
-                        <a href="">
-                            <FaYoutube className='text-black size-8' />
-                        </a>
-                </div>
-                
-
-
-        </div>
+export function Footer() {
+  return (
+    <div className="relative w-full items-center md:pl-5 h-fit mt-10 border-0 border-black flex flex-wrap gap-2.5 md:gap-10">
+      
+    </div>
+  );
 }
